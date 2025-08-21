1a25d60169c67f1110bc2804e8e9b00af5859997
Manoj Pawan Tyagi <tyagimanojtyagi.22@gmail.com>
2025-08-21 19:44:11 +0530

    Refactor: Add Footer component to App.vue for improved site navigation and contact information

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
